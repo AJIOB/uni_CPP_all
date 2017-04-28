368ea76e496a21aa5266f28463eebdc8625701ba
AJIOB <aleksandr9809@gmail.com>
2017-04-28 21:48:50 +0300

    lab6: Good "high order function"-s were made

--- Lab 6. Scala. High order functions/src/main/scala/lab6.scala
+++ Lab 6. Scala. High order functions/src/main/scala/lab6.scala
@@ -8,19 +8,21 @@ import scala.annotation.tailrec
   * @author AJIOB
   */
 object lab6 {
+
+  def isNeedToAdd(c: Array[Int], i: Int) : Array[Int] = {
+    if (c.contains(i))
+      c
+    else
+      c :+ i
+  }
+
   /**
     * Удалить из коллекции все повторяющиеся элементы
     * Delete repeated elements from collection
     */
-  def deleteRepeated() : Array[Int] => Array[Int] = {
-    def isNeedToAdd(c: Array[Int], i: Int) : Array[Int] = {
-      if (c.contains(i))
-        c
-      else
-        c :+ i
-    }
+  def deleteRepeated(p: ((Array[Int], Int) => Array[Int])) : Array[Int] => Array[Int] = {
     def help(c: Array[Int]) : Array[Int] = {
-      c.foldLeft(Array[Int]())(isNeedToAdd)
+      c.foldLeft(Array[Int]())(p)
     }
 
     help
--- Lab 6. Scala. High order functions/src/test/scala/tests.scala
+++ Lab 6. Scala. High order functions/src/test/scala/tests.scala
@@ -17,18 +17,18 @@ import scala.collection.immutable.SortedMap
 @RunWith(classOf[JUnitRunner])
 class tests extends FunSuite {
   test("empty") {
-    assert(lab6.deleteRepeated()(Array[Int]()) sameElements Array[Int]())
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array[Int]()) sameElements Array[Int]())
   }
   test("one element") {
-    assert(lab6.deleteRepeated()(Array(1)) sameElements Array(1))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1)) sameElements Array(1))
   }
   test("non repeated") {
-    assert(lab6.deleteRepeated()(Array(1,4,2,6,3)) sameElements Array(1,4,2,6,3))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1,4,2,6,3)) sameElements Array(1,4,2,6,3))
   }
   test("repeated") {
-    assert(lab6.deleteRepeated()(Array(1,4,2,6,2,3)) sameElements Array(1,4,2,6,3))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1,4,2,6,2,3)) sameElements Array(1,4,2,6,3))
   }
   test("many repeated") {
-    assert(lab6.deleteRepeated()(Array(1,1,2,4,2,6,5,1,4,3)) sameElements Array(1,2,4,6,5,3))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1,1,2,4,2,6,5,1,4,3)) sameElements Array(1,2,4,6,5,3))
   }
 }
