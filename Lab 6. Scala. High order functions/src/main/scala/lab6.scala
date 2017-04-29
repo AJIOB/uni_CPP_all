633b4edad4f8fe8f91ecba2dc83677df732a70a5
AJIOB <aleksandr9809@gmail.com>
2017-04-29 19:00:29 +0300

    lab6: All is ready

--- Lab 6. Scala. High order functions/src/main/scala/lab6.scala
+++ Lab 6. Scala. High order functions/src/main/scala/lab6.scala
@@ -1,30 +1,48 @@
 package main.scala
 
-import scala.annotation.tailrec
-
 /**
   * Created by AJIOB on 18.04.2017.
   *
   * @author AJIOB
   */
 object lab6 {
-
-  def isNeedToAdd(c: Array[Int], i: Int) : Array[Int] = {
+  /**
+    * Удалить из коллекции все повторяющиеся элементы
+    * Delete repeated elements from collection
+    */
+  def isNeedToAdd(c: Array[Int], i: Int): Array[Int] = {
     if (c.contains(i))
       c
     else
       c :+ i
   }
 
-  /**
-    * Удалить из коллекции все повторяющиеся элементы
-    * Delete repeated elements from collection
-    */
-  def deleteRepeated(p: ((Array[Int], Int) => Array[Int])) : Array[Int] => Array[Int] = {
-    def help(c: Array[Int]) : Array[Int] = {
+  def deleteRepeated(p: ((Array[Int], Int) => Array[Int])): Array[Int] => Array[Int] = {
+    def help(c: Array[Int]): Array[Int] = {
       c.foldLeft(Array[Int]())(p)
     }
 
     help
+  }
+
+  /**
+    * Count number of sublists in list
+    *
+    * @param l    Requred list
+    * @param subl Requred sublist
+    * @tparam A Requred type
+    * @return Number of entries
+    */
+  def entries[A](l: List[A], subl: List[A]): Int = {
+    if (subl.isEmpty || l.isEmpty || l.length < subl.length)
+      0
+    else {
+      (0 to (l.length - subl.length)).foldLeft(0)((res, i) => {
+        if (l.slice(i, i + subl.length) == subl)
+          res + 1
+        else
+          res
+      })
+    }
   }
 }
--- Lab 6. Scala. High order functions/src/test/scala/tests.scala
+++ Lab 6. Scala. High order functions/src/test/scala/tests.scala
@@ -6,13 +6,10 @@
 
 package test.scala
 
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSuite
 import main.scala._
-import org.junit.runners.Parameterized.Parameter
-
-import scala.collection.immutable.SortedMap
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class tests extends FunSuite {
@@ -23,12 +20,32 @@ class tests extends FunSuite {
     assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1)) sameElements Array(1))
   }
   test("non repeated") {
-    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1,4,2,6,3)) sameElements Array(1,4,2,6,3))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1, 4, 2, 6, 3)) sameElements Array(1, 4, 2, 6, 3))
   }
   test("repeated") {
-    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1,4,2,6,2,3)) sameElements Array(1,4,2,6,3))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1, 4, 2, 6, 2, 3)) sameElements Array(1, 4, 2, 6, 3))
   }
   test("many repeated") {
-    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1,1,2,4,2,6,5,1,4,3)) sameElements Array(1,2,4,6,5,3))
+    assert(lab6.deleteRepeated(lab6.isNeedToAdd)(Array(1, 1, 2, 4, 2, 6, 5, 1, 4, 3)) sameElements Array(1, 2, 4, 6, 5, 3))
+  }
+
+
+  test("Entries: empty") {
+    assert(lab6.entries(Nil, Nil) == 0)
+  }
+  test("Entries: second is empty") {
+    assert(lab6.entries(List(0, 1, 2), Nil) == 0)
+  }
+  test("Entries: second has one element") {
+    assert(lab6.entries(List(0, 1, 2, 1), List(1)) == 2)
+  }
+  test("Entries: second has two element") {
+    assert(lab6.entries(List(0, 1, 2, 1), List(1, 2)) == 1)
+  }
+  test("Entries: second has many element") {
+    assert(lab6.entries(List(0, 1, 2, 1, 1, 2, 1, 0), List(2, 1)) == 2)
+  }
+  test("Entries: duplicates") {
+    assert(lab6.entries(List(2, 1, 2, 1, 2, 1, 2), List(2, 1, 2)) == 3)
   }
 }
